9effb47c80310e19d803e553a15ccc27d1dee053
Laust <lafa16ac@student.cbs.dk>
2018-04-17 11:58:25 +0200

    virkende excel udtræk

--- js/admin.js
+++ js/admin.js
@@ -1,34 +1,40 @@
 $(document).ready(() => {
 
-    const debug = true;
+    const debugLoad = false;
+    const debugExcel = false;
 
     const welcomeHeader = $("#welcomeHeader");
     const user = JSON.parse(sessionStorage.getItem("currentUser"));
     welcomeHeader.append(user.nameUser);
 
-    let aktiveProdukter = null;
+    let aktiveProdukter = [];
     let aktiveBrugere = null;
     let altKoebt = null;
+
     SDK.Admin.loadAllProducts((callback, data) => {
         if (callback) {
             throw callback;
         }
-        aktiveProdukter = data;
-        debug && console.log(aktiveProdukter);
+        for (let i = 0; i < data.length; i++) {
+            const aktivtProdukt = {idProduct: data[i].idProduct, nameProduct: data[i].nameProduct};
+            aktiveProdukter.push(aktivtProdukt)
+        }
+
+        debugLoad && console.log(aktiveProdukter);
     });
     SDK.Admin.loadEverythingBought((callback, data) => {
         if (callback) {
             throw callback;
         }
         altKoebt = data;
-        debug && console.log(altKoebt);
+        debugLoad && console.log(altKoebt);
     });
     SDK.Admin.loadAllUsers((callback, data) => {
         if (callback) {
             throw callback;
         }
         aktiveBrugere = data;
-        debug && console.log(aktiveBrugere);
+        debugLoad && console.log(aktiveBrugere);
     });
 
     /** Only to be used if the above 3 functions prove too much for the server / browser to handle at large amounts of data
@@ -60,15 +66,45 @@ $(document).ready(() => {
             console.log(aktiveBrugere);
         });
     });*/
+
     $("#exportExcel").click(() => {
 
-        let data = [{city: "Minsk", population: 100000}, {city: "Riga", population: 200000}];
+        let data = [];
 
-        alasql('SELECT * INTO XLSX("exceludtræk.xlsx",{headers:true}) FROM ? ', [data]);
+        for (let i = 0; i < aktiveBrugere.length; i++) {
+            const userRFID = aktiveBrugere[i].RFIDUser;
+            const userName = aktiveBrugere[i].nameUser;
+            let existing = data.find(user => user.name === userName);
+            let index = data.indexOf(existing);
+            if (index === -1) {
+                debugExcel && console.log("creating new user entry");
+                data.push({name: userName, RFID: userRFID});
+                existing = data.find(user => user.name === userName);
+                index = data.indexOf(existing);
+            }
+            for (let i = 0; i < altKoebt.length; i++) {
+                if (altKoebt[i].RFIDUser === userRFID) {
+                    const nameProduct = altKoebt[i].nameProduct;
+                    const amountBought = altKoebt[i].amountBought;
+                    if (!data[index][nameProduct]) {
+                        debugExcel && console.log("creating new product entry");
+                        data[index][nameProduct] = amountBought;
+                    } else {
+                        data[index][nameProduct] += amountBought;
+                    }
+                }
+            }
 
+            alasql('SELECT * INTO XLSX("kiosk_udtræk.xlsx",{headers:true}) FROM ? ', [data]);
+
+            /** Example data to showcase alasql functionality
+             let oldData = [{city: "Minsk", population: 100000}, {city: "Riga", population: 200000}];
+             console.log(oldData);
+             alasql('SELECT * INTO XLSX("cities.xlsx",{headers:true}) FROM ? ', [oldData]);
+             */
+        }
     });
-
-    //lets the user logout
+//lets the user logout
     $("#logoutButton").click(() => {
         SDK.Student.logOut();
     });
